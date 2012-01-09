b945222b4fa9d4e53e807676778f1fad0f7b566a
Radek Stepan <radek.stepan@gmail.com>
2012-01-09 15:21:35 +0000

    add a new underscore js file for testing

---
+++ new-underscore.js
@@ -0,0 +1,13 @@
+var _ = function() {
+
+	return {
+		// Iterates over a list of elements, yielding each in turn to an iterator function. The iterator is
+		//  bound to the context object, if one is passed. Each invocation of iterator is called with three
+		// arguments: (element, index, list). If list is a JavaScript object, iterator's arguments will be
+		// (value, key, list).
+		each: function(list, iterator, context) {
+			
+		}
+	};
+	
+}();
